5806b925e0361986df4de16e682ddc7e96602537
MMAREK1 <martin.mat.marek@gmail.com>
2015-08-19 09:16:44 +0200

    zmeny 19.8 rucne na stranke

--- src/register/ListRegister.java
+++ src/register/ListRegister.java
@@ -26,23 +26,23 @@ public class ListRegister implements Register {
 
 	@Override
 	public Person findPersonByName(String name) {
-//		for (int i = 0; i < persons.size(); i++) {
-//			if (name.equals(persons.get(i).getName())) {
-//				return getPerson(i);
-//			}
-//		}
-//		return null;
+		// for (int i = 0; i < persons.size(); i++) {
+		// if (name.equals(persons.get(i).getName())) {
+		// return getPerson(i);
+		// }
+		// }
+		// return null;
 		return (Person) persons.stream().filter(s -> s.getName().equals(name)).findFirst().orElse(null);
 	}
 
 	@Override
 	public Person findPersonByPhoneNumber(String phoneNumber) {
-//		for (int i = 0; i < persons.size(); i++) {
-//			if (phoneNumber.equals(persons.get(i).getPhoneNumber())) {
-//				return getPerson(i);
-//			}
-//		}
-//		return null;
+		// for (int i = 0; i < persons.size(); i++) {
+		// if (phoneNumber.equals(persons.get(i).getPhoneNumber())) {
+		// return getPerson(i);
+		// }
+		// }
+		// return null;
 		return (Person) persons.stream().filter(s -> s.getPhoneNumber().equals(phoneNumber)).findFirst().orElse(null);
 	}
 
